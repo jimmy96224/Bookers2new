a43b0ab8def42b35bf537ed074503da076223d45
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-09-22 01:56:22 +0000

    [add]ch14 userinfo+userimg

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+  
+  # get 'users/show'
+  # get 'users/edit'
+  resources :users, only: [:show, :edit]
+  
   root to: "homes#top"
+  
   get '/home/about', to: 'homes#about', as: 'about'
+  
 end
