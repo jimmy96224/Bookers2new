67c540e4f4d463bc18cf6ed3a18ac6e866087723
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-09-22 15:53:06 +0000

    [wip]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,28 @@
 class UsersController < ApplicationController
+  def new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "You have created book successfully."
+    redirect_to user_path(@user.id)
+    else
+    # @books = Book.all
+    # render :index
+    end
+  end
+  
+  def show
+    @book = Book.new
+  end
+  
   def index
     @user = User.new
     @users = User.all
   end
   
-  def create
-    
-  end
 
-  def show
-    @book = Book.new(book_params)
-  end
 
   def edit
   end
